ada5ee12da2b35d9237a25b42bade4f3ade5096e
Gandara Fajar Umbara <fajar@umbara.me>
2024-05-14 00:16:55 +0700

    Update Card.stories.ts

--- src/components/card/Card.stories.ts
+++ src/components/card/Card.stories.ts
@@ -14,6 +14,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Example: Story = {
   args: {
-    content: "A Button!",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis vehicula venenatis. Donec finibus nunc diam, eu facilisis lacus fermentum et. Sed fringilla feugiat leo, at faucibus mi facilisis at. Nam nec eros enim. Aliquam et tempus magna. Curabitur condimentum feugiat tempor. Morbi vitae faucibus ipsum. Ut vitae nisi sed risus consequat gravida. In sed consectetur enim, ac consectetur nunc. Donec porta purus euismod dolor elementum sollicitudin. Etiam vel pharetra urna. Donec tortor mauris, dictum vel vulputate sed, sodales luctus dolor.",
   }
 };
